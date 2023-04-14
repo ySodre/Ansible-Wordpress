b0dddefbff176ffffde1a7f509f68714ac2d62e0
Yuri Sodre <sodre-yuri@hotmail.com>
2023-04-14 17:13:27 -0300

    Melhorando os comentarios

--- scripts/asb-script.sh
+++ scripts/asb-script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#apt update -y && apt upgrade -y
-#apt install ansible -y
-#apt install python3 -y
+apt update -y && apt upgrade -y
+apt install ansible -y
+apt install python3 -y
 #Aplicação
-echo -e "Host 192.168.10.105\n  StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
-#Banco de dados
-echo -e "Host 192.168.10.106\n  StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
-chmod 600 /home/vagrant/.ssh/vagrant_key
+
--- scripts/db-script.sh
+++ scripts/db-script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#apt update -y && apt upgrade -y
-echo  "$(cat /home/vagrant/.ssh/vagrant_pub_key)" >> /home/vagrant/.ssh/authorized_keys 
+apt update -y && apt upgrade -y
